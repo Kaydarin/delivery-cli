1c366ab499f2c6f22a8cdaf6f3d004cb5e0778ea
Kaydarin <zim50_killerz@yahoo.com>
2022-08-21 10:34:52 +0800

    Created test input function.

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,24 @@
 #!/usr/bin/env node
 
+const readline = require("readline");
+
 function hello() {
-	console.log("hello world!");
+	// console.log("hello world!");
+
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+
+	rl.question("Hello", (input) => {
+		console.log(input);
+		rl.close();
+	});
+
+	rl.on("close", function () {
+		console.log("\nBYE BYE !!!");
+		process.exit(0);
+	});
 }
 
 hello();
