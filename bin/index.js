eef65561350844fb76d9236d5980a8f8fa4ef485
Kaydarin <zim50_killerz@yahoo.com>
2022-08-21 14:09:28 +0800

    Created test script along with first prompt function.

--- bin/index.js
+++ bin/index.js
@@ -1,24 +1,64 @@
 #!/usr/bin/env node
 
 const readline = require("readline");
+class IO {
+	constructor() {
+		this.rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		});
 
-function hello() {
-	// console.log("hello world!");
+		this.rl.on("close", function () {
+			console.log("\nBYE BYE !!!");
+			process.exit(0);
+		});
+	}
 
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout,
-	});
-
-	rl.question("Hello", (input) => {
-		console.log(input);
-		rl.close();
-	});
-
-	rl.on("close", function () {
-		console.log("\nBYE BYE !!!");
-		process.exit(0);
-	});
+	isNumExist = (val) => {
+		if (isNaN(val) || typeof val == "undefined") {
+			return false;
+		} else {
+			return true;
+		}
+	};
 }
 
-hello();
+class App extends IO {
+	constructor() {
+		super();
+	}
+
+	start = () => {
+		this.rl.question(
+			"What is your base delivery cost & how many packages? Example: BASE_COST <SPACE> NO_OF_PACKAGES : ",
+			this.inputBaseCostAndPackagesNo
+		);
+	};
+
+	inputBaseCostAndPackagesNo = async (input) => {
+		try {
+			const words = input.split(" ");
+
+			const baseCost = parseInt(words[0]);
+			const packageCount = parseInt(words[1]);
+
+			if (!this.isNumExist(baseCost)) {
+				throw "Base cost input must exist and should be number";
+			}
+
+			if (!this.isNumExist(packageCount)) {
+				throw "No. of packages input must exist and should be number";
+			}
+
+			// TODO: Loop through no. of packages and ask each package info (package ID, package weight, package distance, discount code)
+
+			this.rl.close();
+		} catch (e) {
+			console.log("\nERROR:", e);
+			this.rl.close();
+		}
+	};
+}
+
+const app = new App();
+app.start();
--- test/index.test.js
+++ test/index.test.js
@@ -5,7 +5,28 @@ describe("Delivery CLI Test", () => {
 		done();
 	});
 
-	it("Runs successfully", async () => {
+	const initialQuestion = `What is your base delivery cost & how many packages? Example: BASE_COST <SPACE> NO_OF_PACKAGES :`;
+	const baseCostInputError =
+		"ERROR: Base cost input must exist and should be number";
+	const packagesCountInputError =
+		"ERROR: No. of packages input must exist and should be number";
+	const quitMessage = "BYE BYE !!!";
+
+	const successDataSet = [
+		[100, 3],
+		[0, 10],
+		[1, 1],
+		[20, 0],
+	];
+
+	const failureDataSet = [
+		[undefined, 10, baseCostInputError],
+		[100, undefined, packagesCountInputError],
+		["text", 0, baseCostInputError],
+		[10, "text", packagesCountInputError],
+	];
+
+	it.each(successDataSet)("Runs successfully", async (inputOne, inputTwo) => {
 		const { spawn, cleanup } = await prepareEnvironment();
 		const {
 			waitForText,
@@ -20,20 +41,59 @@ describe("Delivery CLI Test", () => {
 
 		debug(); // enables logging to console from the tested program
 
-		const inputText = "world";
+		const inputText = `${inputOne} ${inputTwo}`;
 
-		await waitForText("Hello"); // wait for question
+		await waitForText(initialQuestion); // wait for question
 		await writeText(inputText); // answer the question above
 		await pressKey("enter"); // confirm with Enter
 		await waitForFinish(); // wait for program to finish
 
 		kill(); // would kill the program if we didn't wait for finish above
 
-		expect(getStdout()).toStrictEqual(["Hello", inputText, "BYE BYE !!!"]);
+		expect(getStdout()).toStrictEqual([initialQuestion, quitMessage]);
 		expect(getExitCode()).toBe(0);
 
 		await cleanup(); // cleanup after test
 	});
+
+	it.each(failureDataSet)(
+		"Failed with first input: %s and second input: %s",
+		async (inputOne, inputTwo, expectedMessage) => {
+			const { spawn, cleanup } = await prepareEnvironment();
+			const {
+				waitForText,
+				waitForFinish,
+				writeText,
+				getStdout,
+				getExitCode,
+				kill,
+				debug,
+				pressKey,
+			} = await spawn("node", "./bin/index.js");
+
+			debug();
+
+			const inputText = `${inputOne} ${inputTwo}`;
+
+			await waitForText(initialQuestion);
+			if (!isNaN(inputOne)) {
+				await writeText(inputText);
+			}
+			await pressKey("enter");
+			await waitForFinish();
+
+			kill();
+
+			expect(getStdout()).toStrictEqual([
+				initialQuestion,
+				expectedMessage,
+				quitMessage,
+			]);
+			expect(getExitCode()).toBe(0);
+
+			await cleanup();
+		}
+	);
 
 	afterAll((done) => {
 		done();
